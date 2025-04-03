0e0921591c0f78771f597a092a9844ca8f777204
Ariel Ch <dominicus.arielch@gmail.com>
2025-04-03 21:56:26 +0700

    under construction page
    (cherry picked from commit 1e20434feb10abf608b50973aba673606bb51b98)
    
    init
    
    (cherry picked from commit f7fa7b344503535bd9a3ab8ef186d50b7aca7d55)

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen flex justify-center items-center bg-gray-50">
+    <h1 class="text-5xl font-bold text-gray-800">
+      Under Construction 🚧<span class="animate-blink">|</span>
+    </h1>
+  </div>
+</template>
+
+<style>
+@keyframes blink {
+  50% {
+    opacity: 0;
+  }
+}
+
+.animate-blink {
+  animation: blink 1s step-end infinite;
+}
+</style>
